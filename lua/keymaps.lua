d9f9da6084ab856f41177d50dda122588724f88c
HP-Python <hp900809@163.com>
2024-01-04 11:57:13 +0800

    h

---
+++ lua/keymaps.lua
@@ -0,0 +1,24 @@
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+-- 
+local keymap = vim.keymap
+-- remap space as leader key
+keymap.set("", "<Space>", '<Nop>', opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+------------- insert mode -------------
+keymap.set('i', 'jk', '<ESC>', opts)
+------------- normal mode -------------
+-- window jump
+keymap.set('n', '<leader>h', '<C-w>h', opts)
+keymap.set('n', '<leader>j', '<C-w>j', opts)
+keymap.set('n', '<leader>l', '<C-w>l', opts)
+keymap.set('n', '<leader>k', '<c-w>k', opts)
+---------  visaul mode ------------
+-- '< first line of the last selection
+-- +1 position 
+-- move line
+-- '> last line of the last selection
+keymap.set('v', 'J', ":m '>+1<CR>gv=gv", opts)
+keymap.set('v', 'K', ":m '<-2<CR>gv=gv", opts)
---
+++ lua/options.lua
@@ -0,0 +1,48 @@
+local opt = vim.opt
+
+-- : help options
+opt.backup = false
+opt.clipboard = "unnamedplus"
+opt.cmdheight = 2
+opt.completeopt = { "menuone", "noselect" }
+opt.fileencoding = "utf-8"
+opt.ignorecase = true
+opt.smartcase = true
+opt.showmode = true
+opt.showtabline = 2
+opt.smartindent = true
+opt.splitbelow = true
+opt.splitright = true
+opt.updatetime = 300
+opt.writebackup = false
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+-- scroll
+opt.scrolloff = 8 -- usefull
+opt.sidescrolloff = 8
+opt.guifont = "monospace:h17"
+
+opt.shortmess:append "c"
+
+opt.whichwrap:append( "<,>,[,],h,l" )
+
+-- UI config
+opt.relativenumber = true
+opt.number = true
+opt.numberwidth = 2
+opt.termguicolors = true
+opt.signcolumn = "yes"
+opt.cursorline = true -- highlight the cursor line
+opt.wrap = false
+
+
+
+-- window split
+opt.splitright = true
+opt.splitbelow = true
+
+-- search
+
+
+
